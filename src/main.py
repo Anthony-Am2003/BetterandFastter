ac9dc962820d54790e23c40687a27de7dcabe06c
Anthony-Am2003 <tonyaamb2003@gmail.com>
2024-01-19 11:09:51 -0500

    requirements apps addeds and fastAPI configuration

--- src/main.py
+++ src/main.py
@@ -1,7 +1,11 @@
-from fastapi import FastAPI, HTTPExeception
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
+from fastapi.responses import RedirectResponse
 
 app = FastAPI()
+
+#middleware settings
 
 app.add_middleware(
     CORSMiddleware,
@@ -11,4 +15,8 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+#redirect to /docs automatically
 
+@app.get('/', response_class=RedirectResponse, include_in_schema=False)
+async def docs():
+    return RedirectResponse(url='/docs')
